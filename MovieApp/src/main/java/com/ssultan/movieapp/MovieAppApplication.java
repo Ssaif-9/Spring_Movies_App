8c34775b607f209fd6d24b1e0e5bf1c27ceeacce
Ssaif-9 <116103264+Ssaif-9@users.noreply.github.com>
2024-12-08 20:40:37 +0200

    Deploy with Swagger UI http://localhost:8080/swagger-ui/index.html#/

--- MovieApp/src/main/java/com/ssultan/movieapp/MovieAppApplication.java
+++ MovieApp/src/main/java/com/ssultan/movieapp/MovieAppApplication.java
@@ -1,11 +1,26 @@
 package com.ssultan.movieapp;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class})
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Movie App",
+                description = "Movie App Using OMDB API ",
+                version = "v1.0",
+                contact = @Contact(
+                        name = "saif sultan",
+                        email = "saifsoltan18@gmail.com",
+                        url ="https://github.com/Ssaif-9/Spring_Movies_App"
+                )
+        )
+)
 public class MovieAppApplication {
 
     public static void main(String[] args) {
